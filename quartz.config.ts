59ac72ec9d6bf3df9592991696aadb04ec30adb5
Matt Hamm <matthamm941@gmail.com>
2024-10-04 16:46:32 -0700

    Quartz sync: Oct 4, 2024, 4:46 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🌌 Guilded in Shadow",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
